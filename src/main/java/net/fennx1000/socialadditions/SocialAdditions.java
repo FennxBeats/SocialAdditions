2535d44ab344ceb11eb9987def24c9be4f1b0d35
Fennx1000 <fennwalkinshaw@outlook.com>
2025-11-29 17:32:39 +0000

    Initial commit

---
+++ src/main/java/net/fennx1000/socialadditions/SocialAdditions.java
@@ -0,0 +1,17 @@
+package net.fennx1000.socialadditions;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SocialAdditions implements ModInitializer {
+	public static final String MOD_ID = "socialadditions";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/fennx1000/socialadditions/SocialAdditionsClient.java
@@ -0,0 +1,10 @@
+package net.fennx1000.socialadditions;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SocialAdditionsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+    }
+}
