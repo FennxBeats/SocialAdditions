df1cdd22e83f998d4a7e0b59b5ee5fe3e79ba4a0
Fennx1000 <fennwalkinshaw@outlook.com>
2025-11-30 11:46:20 +0000

    started

--- src/main/java/net/fennx1000/socialadditions/item/ModItems.java
+++ src/main/java/net/fennx1000/socialadditions/item/ModItems.java
@@ -6,27 +6,29 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(
-                Registries.ITEM,
-                Identifier.of(SocialAdditions.MOD_ID, name),
-                item
-        );
+    private static Item registerItem(String name, Item.Settings itemSettings) {
+        Identifier id = Identifier.of(SocialAdditions.MOD_ID, name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item.Settings settings = itemSettings.registryKey(key);
+
+        return Registry.register(Registries.ITEM, key, new Item(settings));
     }
 
     public static final Item RANDOM_POTION = registerItem(
             "random_potion",
-            new Item(new Item.Settings().maxCount(1))
+            new Item.Settings().maxCount(1)
     );
 
 
     public static void registerModItems() {
         SocialAdditions.LOGGER.info("Registering Mod Items for " + SocialAdditions.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register(entries -> {
             entries.add(RANDOM_POTION);
         });
     }
